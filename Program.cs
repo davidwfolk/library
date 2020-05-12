4a7d8b2f7b9091052926265a5530790f4442ef83
David Folk <davidwfolk@gmail.com>
2020-05-12 14:58:25 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using console_library.Models;
+
+namespace library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.Author = "Shel Silverstein";
+            myBook.Title = "Where The Sidewalk Ends";
+        }
+    }
+}
---
+++ models/Book.cs
@@ -0,0 +1,16 @@
+namespace console_library.Models {
+  public class Book {
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public bool Available { get; set; }
+
+  
+  public Book(string title, string author)
+  {
+    Title = title;
+    Author = author;
+    Available = true;
+  }
+  }
+
+}
